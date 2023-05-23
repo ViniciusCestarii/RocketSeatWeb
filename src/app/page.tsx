9fc2fc2e6e66900c39baa92fe3b2c15c7ce791bf
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-05-23 10:49:57 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="m-auto text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
+}
+
+export default Home
