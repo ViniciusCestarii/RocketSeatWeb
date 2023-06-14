29de14e48d5c6c7abc63be2c6ac5e5ad18cb0eb7
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-06-14 20:48:13 -0300

    Create memories list

--- src/components/EmptyMemories.tsx
+++ src/components/EmptyMemories.tsx
@@ -1,6 +1,6 @@
 const EmptyMemories = () => {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[350px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, comece a{' '}
         <a href="" className="underline hover:text-gray-50">
