c38caeb533fa126e93333adfdafcf499aff4ee27
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-08-30 14:39:56 -0300

    configuring next to accept localhost image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '192.168.2.116'],
+    domains: ['avatars.githubusercontent.com', '192.168.2.116', '127.0.0.1'],
   },
 }
 
